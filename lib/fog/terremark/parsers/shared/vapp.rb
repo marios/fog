cf745b2e68cdd91f7c2b0ba0d14097bac2223433
marios <marios@redhat.com>
2010-05-04 15:33:03 +0100

    [terremark] fix parsers/vapp.rb to grab cpu, ram and disks: @response['cpu'] => "" @response['ram'] => "" @response['disks'] => [""]

--- lib/fog/terremark/parsers/shared/vapp.rb
+++ lib/fog/terremark/parsers/shared/vapp.rb
@@ -6,9 +6,14 @@ module Fog
         class Vapp < Fog::Parsers::Base
 
           def reset
-            @response = { 'Links' => [] }
+            @response = { 'Links' => [], 'disks' => []}
+            @inside_os_section = false
+            @inside_item = false
+            @get_cpu = false
+            @get_ram = false
+            @get_disks = false
           end
-
+ 
           def start_element(name, attributes)
             @value = ''
             case name
@@ -18,7 +23,9 @@ module Fog
                   link[attributes.shift] = attributes.shift
                 end
                 @response['Links'] << link
-              when 'Vapp'
+              when 'OperatingSystemSection'
+                @inside_os_section = true
+             when 'VApp'
                 vapp = {}
                 until attributes.empty?
                   if attributes.first.is_a?(Array)
@@ -29,14 +36,46 @@ module Fog
                   end
                 end
                 @response.merge!(vapp.reject {|key,value| !['href', 'name', 'size', 'status', 'type'].include?(key)})
-            end
+             when 'Item'
+                @inside_item = true
+             end
           end
 
           def end_element(name)
             case name
-            when 'IpAddress'
-              @response['IpAddress'] = @value
+              when 'IpAddress'
+                @response['IpAddress'] = @value
+              when 'Description'
+                if @inside_os_section
+                  @response['os'] = @value
+                  @inside_os_section = false
+                end
+              when 'ResourceType'
+                if @inside_item
+                    case @value
+                      when '3' 
+                        @get_cpu = true # cpu
+                      when '4'  # memory
+                        @get_ram = true
+                      when '17' # disks
+                        @get_disks = true
+                    end   
+                end
+              when 'VirtualQuantity'
+                if (@get_cpu)
+                  @response['cpu'] = @value
+                  @get_cpu = false
+                elsif (@get_ram)                
+                  @response['ram'] = @value
+                  @get_ram = false
+                elsif (@get_disks)
+                  @response['disks'] << @value  
+                  @get_disks = false
+                end
+              when 'Item'
+                @inside_item = false
             end
+            
           end
 
         end
@@ -45,3 +84,4 @@ module Fog
     end
   end
 end
+
